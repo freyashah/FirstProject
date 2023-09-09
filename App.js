3c352832c737d48c5fddc3fe79d2cb03b1b9b526
freyashah <freyashah12@gmail.com>
2023-09-09 21:42:05 +0530

    Different types of data show in UI

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
-
+let age = 39;
+var email = 'xyz@gmail.com';
 const App = () => {
+  const name = 'sima'
   return (
     <View style={styles.container}>
       <Text>Welcome to freya's app.Hii!!  from freya</Text>
+      <Text>{name}</Text>
+      <Text>{age}</Text>
+      <Text>{email}</Text>
+      <Text>{fruit()}</Text>
+      <Text>{3*2}</Text>
+      <Text>{age === 39 ? 'OldAge' : 'YoungAge' }</Text>
       <Button title="Press Here"></Button>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
+function fruit(){
+  return 'apple';
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: 'pink',
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
   },
