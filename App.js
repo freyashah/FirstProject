9e9a200065548381e915f45b21c87b828faa9a07
freyashah <freyashah12@gmail.com>
2023-09-09 21:17:54 +0530

    Added Button

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
       <Text>Welcome to freya's app.Hii!!  from freya</Text>
+      <Button title="Press Here"></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+export default App;
