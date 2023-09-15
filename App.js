0e6406c1f9feba7cece050c48469230f12e46648
freyashah <freyashah12@gmail.com>
2023-09-15 22:09:50 +0530

    Custom list

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput, FlatList } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, FlatList, ScrollView } from 'react-native';
 import FirstComponent from './components/FirstComponent';
 import { useState } from 'react';
 import ExStyles from './css/MainCSS';
@@ -20,6 +20,14 @@ const App = () => {
     id: 2,
     name: 'dhruval'
   }]
+  const emails = [{
+    id: 1,
+    text: 'freya@test.com'
+  },
+  {
+    id: 2,
+    text: 'dhruval@test.com'
+  },]
   function ChangeName() {
     SetName('FreyaDShah')
   }
@@ -33,13 +41,19 @@ const App = () => {
       <CustomButton buttonTitle="Press Here" />
       <Button title="Update Name" onPress={ChangeName}></Button>
       <FirstComponent name={GetName} />
-      <TextInput style={ExStyles.textinput} value={GetName} placeholder='Enter Your Name' onChangeText={(text) => SetName(text)} />
+      <TextInput style={ExStyles.textinput} value={GetName} placeholder='Enter Your Name'
+        onChangeText={(text) => SetName(text)} />
       <CustomButton buttonTitle="Clear Input" />
-      <TextInput style={ExStyles.textinput} value={GetPassword} placeholder='Enter Password' onChangeText={(text) => Setpassword(text)}
+      <TextInput style={ExStyles.textinput} value={GetPassword} placeholder='Enter Password'
+        onChangeText={(text) => Setpassword(text)}
         secureTextEntry={DisplayPassword} />
       <Button title='View Password' onPress={() => SetDisplayPassword(!DisplayPassword)}></Button>
       <Button title='Clear Password' onPress={() => Setpassword('')}></Button>
-      <FlatList data={users} renderItem={({ item }) => <Text style={styles.textbox}>{item.name}</Text>} keyExtractor={item => item.id} />
+      <FlatList data={users} renderItem={({ item }) => <Text style={styles.textbox}>{item.name}</Text>}
+        keyExtractor={item => item.id} />
+      <ScrollView>{
+        emails.map((item) => <Text>{item.text}</Text>)
+      }</ScrollView>
     </View>
   );
 }
