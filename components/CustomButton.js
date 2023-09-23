b63481ecaaa781c98225cb4a73332def0a189b0e
freyashah <freyashah12@gmail.com>
2023-09-23 20:35:20 +0530

    loader

--- components/CustomButton.js
+++ components/CustomButton.js
@@ -1,15 +1,25 @@
-import { View, Button, StyleSheet } from "react-native"
+import { useState } from "react"
+import { View, Button, StyleSheet, ActivityIndicator } from "react-native"
 
 const ExportButton = (props) => {
+    const [show, setShow] = useState(false);
+    function ButtonPress(val) {
+        console.warn(val)
+        setShow(true)
+        setTimeout(() => {
+            setShow(false)
+        }, 3000)
+    }
     return (
-        <View style={styles.button}>
-            <Button color='white' title={props.buttonTitle} onPress={() => props.buttonTitle === 'Clear Input' ? ClearInput() : ButtonPress("Button is pressed")}></Button>
+        <View>
+            <ActivityIndicator size={'large'} color='black' animating={show} />
+            <View style={styles.button}>
+                <Button color='white' title={props.buttonTitle} onPress={() => props.buttonTitle === 'Clear Input' ? ClearInput() : ButtonPress("Button is pressed")}></Button>
+            </View>
         </View>
     )
 }
-function ButtonPress(val) {
-    console.warn(val)
-}
+
 function ClearInput() {
     console.warn('clear')
 }
