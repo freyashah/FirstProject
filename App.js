bbe74d76e4edf39faf7b894058c8b9e1bc1d0932
freyashah <freyashah12@gmail.com>
2023-09-26 00:01:41 +0530

    open website on button click

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import ExStyles from './css/MainCSS';
 import CustomButton from './components/CustomButton';
 import Student from './components/ClassComponent';
 import RadioButton from './components/RadioButtion';
+import { WebView } from 'react-native-webview'
 
 let age = 39;
 var email = 'xyz@gmail.com';
@@ -14,6 +15,7 @@ const App = () => {
   const [GetName, SetName] = useState('Freya');
   const [GetPassword, Setpassword] = useState('');
   const [DisplayPassword, SetDisplayPassword] = useState(true);
+  const [OpenWebView, SetOpenWebView] = useState(false)
   const users = [{
     id: 1,
     name: 'freya'
@@ -81,69 +83,75 @@ const App = () => {
   useEffect(() => {
     console.warn("state is updated")
   }, [GetName, GetPassword])
+
   return (
     <View style={styles.container}>
-      <View style={styles.box1}>
-        <View style={styles.innerbox1}>
-          <Text>Inner box 1</Text>
-        </View>
-        <View style={styles.innerbox2}>
-          <Text style={styles.OSStyle}>
-            {JSON.stringify(Platform)}
-            {JSON.stringify(Platform.constants.reactNativeVersion.minor)}
-          </Text>
-          {
-            Platform.OS == 'android' ? <Text style={{ backgroundColor: 'blue' }}>hiii {Platform.OS}</Text> :
-              <Text style={{ backgroundColor: 'purple' }}>Hii {Platform.OS}</Text>
-          }
-        </View>
-      </View>
-      <View style={styles.box2}>
-        <View style={styles.innerbox21}>
-          <TouchableHighlight>
-            <Text style={styles.touchablebutton}>Touchable button</Text>
-          </TouchableHighlight>
-          <Pressable onPress={() => console.warn("button pressed")} onLongPress={() => console.warn("long pressed")}
-            onPressIn={() => console.warn("press in")} onPressOut={() => console.warn("press out")}>
-            <Text style={styles.pressableBtn}>Pressable button</Text>
-          </Pressable>
-          <RadioButton />
-        </View>
-        <Text>{Platform.OS}</Text>
-      </View>
-      <View style={styles.box3}>
-        <Text style={[styles.textbox, ExStyles.textbox, { padding: 10, borderRadius: 10 }]}>{name}</Text>
-        <Text>{fruit()}</Text>
-        <Text>{3 * 2}</Text>
-        <Text>{age === 39 ? 'OldAge' : 'YoungAge'}</Text>
-        <Text> State Name : {GetName} </Text>
-        <CustomButton buttonTitle="Press Here" />
-        <Button title="Update Name" onPress={ChangeName}></Button>
-        {
-          GetName == 'Freya' ? <FirstComponent name={GetName} /> : null
-        }
+      {OpenWebView == true
+        ? <WebView source={{ uri: "https://www.hotstar.com/in" }} /> :
+        <View style={styles.container}>
+          <View style={styles.box1}>
+            <View style={styles.innerbox1}>
+              <Text>Inner box 1</Text>
+            </View>
+            <View style={styles.innerbox2}>
+              <Text style={styles.OSStyle}>
+                {JSON.stringify(Platform)}
+                {JSON.stringify(Platform.constants.reactNativeVersion.minor)}
+              </Text>
+              {
+                Platform.OS == 'android' ? <Text style={{ backgroundColor: 'blue' }}>hiii {Platform.OS}</Text> :
+                  <Text style={{ backgroundColor: 'purple' }}>Hii {Platform.OS}</Text>
+              }
+            </View>
+          </View>
+          <View style={styles.box2}>
+            <View style={styles.innerbox21}>
+              <TouchableHighlight onPress={() => SetOpenWebView(true)}>
+                <Text style={styles.touchablebutton}>Touchable button</Text>
+              </TouchableHighlight>
+              <Pressable onPress={() => console.warn("button pressed")} onLongPress={() => console.warn("long pressed")}
+                onPressIn={() => console.warn("press in")} onPressOut={() => console.warn("press out")}>
+                <Text style={styles.pressableBtn}>Pressable button</Text>
+              </Pressable>
+              <RadioButton />
+            </View>
+            <Text>{Platform.OS}</Text>
+          </View>
+          <View style={styles.box3}>
+            <Text style={[styles.textbox, ExStyles.textbox, { padding: 10, borderRadius: 10 }]}>{name}</Text>
+            <Text>{fruit()}</Text>
+            <Text>{3 * 2}</Text>
+            <Text>{age === 39 ? 'OldAge' : 'YoungAge'}</Text>
+            <Text> State Name : {GetName} </Text>
+            <CustomButton buttonTitle="Press Here" />
+            <Button title="Update Name" onPress={ChangeName}></Button>
+            {
+              GetName == 'Freya' ? <FirstComponent name={GetName} /> : null
+            }
 
-        <TextInput style={ExStyles.textinput} value={GetName} placeholder='Enter Your Name'
-          onChangeText={(text) => SetName(text)} />
-        <CustomButton buttonTitle="Clear Input" />
-        <TextInput style={ExStyles.textinput} value={GetPassword} placeholder='Enter Password'
-          onChangeText={(text) => Setpassword(text)}
-          secureTextEntry={DisplayPassword} />
-        <Button title='View Password' onPress={() => SetDisplayPassword(!DisplayPassword)}></Button>
-        <Button title='Clear Password' onPress={() => Setpassword('')}></Button>
-        <FlatList data={users} horizontal={true} renderItem={({ item }) => <Text style={styles.textbox}>{item.name}</Text>}
-          keyExtractor={item => item.id} />
-        <ScrollView>
-          <View style={styles.grid}>{
-            emails.map((item, index) => <Text key={index} style={styles.griditem}>{item.text}</Text>)
-          }</View>
-        </ScrollView>
-        <FlatList data={newemails} horizontal={true} renderItem={({ item }) => <GridData itemlist={item} />}
-          keyExtractor={item => item.id} />
-        <SectionList horizontal={true} sections={studentreport} renderItem={({ item }) => <Text>{item.subject} - {item.marks} </Text>}
-          renderSectionHeader={({ section: { name } }) => <Text>{name}</Text>} />
-        <Student name={GetName} />
-      </View>
+            <TextInput style={ExStyles.textinput} value={GetName} placeholder='Enter Your Name'
+              onChangeText={(text) => SetName(text)} />
+            <CustomButton buttonTitle="Clear Input" />
+            <TextInput style={ExStyles.textinput} value={GetPassword} placeholder='Enter Password'
+              onChangeText={(text) => Setpassword(text)}
+              secureTextEntry={DisplayPassword} />
+            <Button title='View Password' onPress={() => SetDisplayPassword(!DisplayPassword)}></Button>
+            <Button title='Clear Password' onPress={() => Setpassword('')}></Button>
+            <FlatList data={users} horizontal={true} renderItem={({ item }) => <Text style={styles.textbox}>{item.name}</Text>}
+              keyExtractor={item => item.id} />
+            <ScrollView>
+              <View style={styles.grid}>{
+                emails.map((item, index) => <Text key={index} style={styles.griditem}>{item.text}</Text>)
+              }</View>
+            </ScrollView>
+            <FlatList data={newemails} horizontal={true} renderItem={({ item }) => <GridData itemlist={item} />}
+              keyExtractor={item => item.id} />
+            <SectionList horizontal={true} sections={studentreport} renderItem={({ item }) => <Text>{item.subject} - {item.marks} </Text>}
+              renderSectionHeader={({ section: { name } }) => <Text>{name}</Text>} />
+            <Student name={GetName} />
+          </View>
+        </View>
+      }
     </View>
   );
 }
