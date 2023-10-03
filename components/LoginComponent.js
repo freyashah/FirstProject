c1032e288ba7178288f94c93accbd1c84288a670
freyashah <freyashah12@gmail.com>
2023-10-03 22:48:10 +0530

    Top navigation

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -2,8 +2,11 @@ import { View, Text, StyleSheet } from 'react-native'
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import { NavigationContainer } from "@react-navigation/native"
 import SignUp from './SignUpComponent'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import Example from './ExampleScreen'
 
 const Tab = createBottomTabNavigator()
+const TopTab = createMaterialTopTabNavigator()
 const Login = (props) => {
     const { name, otherName } = props.route.params
     return (
@@ -14,10 +17,22 @@ const Login = (props) => {
             <NavigationContainer independent={true}>
                 <Tab.Navigator>
                     <Tab.Screen name='SignUp' component={SignUp} />
+                    <Tab.Screen name='Nested' component={Nested} />
                 </Tab.Navigator>
+
             </NavigationContainer>
         </View>
 
+    )
+}
+const Nested = () => {
+    return (
+        <NavigationContainer independent={true}>
+            <TopTab.Navigator>
+                <TopTab.Screen name='Top Signup' component={SignUp} />
+                <TopTab.Screen name='Top Example' component={Example} />
+            </TopTab.Navigator>
+        </NavigationContainer>
     )
 }
 const styles = StyleSheet.create({
