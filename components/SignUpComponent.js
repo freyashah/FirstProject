0496c4b5cc4994f4e2d647239cb5a6efed2bf833
freyashah <freyashah12@gmail.com>
2023-10-03 23:28:25 +0530

    Async api call

--- components/SignUpComponent.js
+++ components/SignUpComponent.js
@@ -1,9 +1,32 @@
-import { View, Text, StyleSheet } from "react-native"
+import { useEffect, useState } from "react"
+import { View, Text, StyleSheet, ScrollView } from "react-native"
+
 
 const SignUp = () => {
+    const [data, setData] = useState(undefined);
+
+    const getAPIMethod = async () => {
+        const url = 'https://jsonplaceholder.typicode.com/posts';
+        let result = await fetch(url);
+        result = await result.json();
+        setData(result)
+    }
+    useEffect(() => {
+        getAPIMethod();
+    }, [])
+
     return (
         <View style={styles.tabstyle}>
             <Text>Signup</Text>
+            {
+                data ? <ScrollView>
+                    <View>{
+                        data.map((item, index) => <Text key={index}>{item.title}</Text>)
+                    }</View>
+                </ScrollView>
+                    : null
+            }
+
         </View>
     )
 }
