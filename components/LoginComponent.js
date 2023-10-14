31bc285e3ac6c6afa0200fa247830654d7e34e33
freyashah <freyashah12@gmail.com>
2023-10-14 22:56:58 +0530

    PostAPI

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import { NavigationContainer } from "@react-navigation/native"
 import SignUp from './SignUpComponent'
@@ -10,6 +10,9 @@ const Tab = createBottomTabNavigator()
 const TopTab = createMaterialTopTabNavigator()
 const Login = (props) => {
     const { name, otherName } = props.route.params
+    const APIdata = {
+        "name": otherName
+    }
     useEffect(() => {
         GetCustomAPIData();
     }, [])
@@ -19,11 +22,23 @@ const Login = (props) => {
         result = await result.json();
         console.warn(result)
     }
+    const PostAPI = async () => {
+        console.warn(APIdata)
+        const url = 'http://192.168.29.140:3000/users'
+        let result = await fetch(url, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(APIdata)
+        })
+        result = await result.json();
+        console.warn(result)
+    }
     return (
         <View style={styles.container}>
             <Text>This is Login screen</Text>
             <Text>Name:{name}</Text>
             <Text>Other Name: {otherName}</Text>
+            <Button title='Pass Data To API' onPress={PostAPI} />
             <NavigationContainer independent={true}>
                 <Tab.Navigator>
                     <Tab.Screen name='SignUp' component={SignUp} />
