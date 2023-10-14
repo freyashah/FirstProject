335a23f60f6b4ad2a1603d2a982ba7255d338b42
freyashah <freyashah12@gmail.com>
2023-10-14 23:52:03 +0530

    Validations on form

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -1,10 +1,10 @@
-import { View, Text, StyleSheet, Button } from 'react-native'
+import { View, Text, StyleSheet, Button, TextInput } from 'react-native'
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import { NavigationContainer } from "@react-navigation/native"
 import SignUp from './SignUpComponent'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 import Example from './ExampleScreen'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 const Tab = createBottomTabNavigator()
 const TopTab = createMaterialTopTabNavigator()
@@ -13,6 +13,8 @@ const Login = (props) => {
     const APIdata = {
         "name": otherName
     }
+    const [age, setAge] = useState('');
+    const [ageError, setAgeError] = useState(false);
     useEffect(() => {
         GetCustomAPIData();
     }, [])
@@ -23,7 +25,11 @@ const Login = (props) => {
         console.warn(result)
     }
     const PostAPI = async () => {
-        console.warn(APIdata)
+        console.warn(age)
+        setAgeError(!age ? true : false)
+        if (!age) {
+            return false;
+        }
         const url = 'http://192.168.29.140:3000/users'
         let result = await fetch(url, {
             method: "POST",
@@ -38,6 +44,8 @@ const Login = (props) => {
             <Text>This is Login screen</Text>
             <Text>Name:{name}</Text>
             <Text>Other Name: {otherName}</Text>
+            <TextInput style={styles.textInput} placeholder='Enter Age' onChangeText={(text) => setAge(text)} value={age} />
+            {ageError ? <Text style={styles.error}>Enter valid age</Text> : null}
             <Button title='Pass Data To API' onPress={PostAPI} />
             <NavigationContainer independent={true}>
                 <Tab.Navigator>
@@ -63,6 +71,14 @@ const Nested = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    textInput: {
+        borderColor: 'skyblue',
+        borderStyle: 'solid',
+        color: 'skyblue'
+    },
+    error: {
+        color: 'red'
     }
 })
 export default Login
