825ff5d44ebc2109a8786ad832a1de345e7cd9b8
freyashah <freyashah12@gmail.com>
2023-10-16 23:00:08 +0530

    Ref in react native

--- components/ExampleScreen.js
+++ components/ExampleScreen.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, TextInput, FlatList, ScrollView, SectionList, TouchableHighlight, Pressable, Platform } from 'react-native';
 import FirstComponent from '../components/FirstComponent'
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import ExStyles from '../css/MainCSS';
 import CustomButton from '../components/CustomButton';
 import Student from '../components/ClassComponent';
@@ -18,6 +18,7 @@ const Example = (props) => {
   const [DisplayPassword, SetDisplayPassword] = useState(true);
   const [OpenWebView, SetOpenWebView] = useState(false);
   const [showCustomModal, setShowCustomModal] = useState(false);
+  const input = useRef();
   const users = [{
     id: 1,
     name: 'freya'
@@ -77,7 +78,12 @@ const Example = (props) => {
     }
   ]
   function ChangeName() {
-    SetName('FreyaDShah')
+    SetName('FreyaDShah');
+    input.current.focus();
+    input.current.setNativeProps({
+      color: '#fff',
+      fontSize: 20
+    })
   }
   useEffect(() => {
     console.warn('Component Mounted')
@@ -87,81 +93,83 @@ const Example = (props) => {
   }, [GetName, GetPassword])
 
   return (
-    <View style={styles.container}>
-      {OpenWebView == true
-        ? <WebView source={{ uri: "https://www.hotstar.com/in" }} /> :
-        <View style={styles.container}>
-          <View style={styles.box1}>
-            <View style={styles.innerbox1}>
-              <Text>Inner box 1</Text>
+    <ScrollView>
+      <View style={styles.container}>
+        {OpenWebView == true
+          ? <WebView source={{ uri: "https://www.hotstar.com/in" }} /> :
+          <View style={styles.container}>
+            <View style={styles.box1}>
+              <View style={styles.innerbox1}>
+                <Text>Inner box 1</Text>
+              </View>
+              <View style={styles.innerbox2}>
+                <Text style={styles.OSStyle}>
+                  {JSON.stringify(Platform)}
+                  {JSON.stringify(Platform.constants.reactNativeVersion.minor)}
+                </Text>
+                {
+                  Platform.OS == 'android' ? <Text style={{ backgroundColor: 'blue' }}>hiii {Platform.OS}</Text> :
+                    <Text style={{ backgroundColor: 'purple' }}>Hii {Platform.OS}</Text>
+                }
+              </View>
             </View>
-            <View style={styles.innerbox2}>
-              <Text style={styles.OSStyle}>
-                {JSON.stringify(Platform)}
-                {JSON.stringify(Platform.constants.reactNativeVersion.minor)}
-              </Text>
+            <View style={styles.box2}>
+              <View style={styles.innerbox21}>
+                <TouchableHighlight onPress={() => SetOpenWebView(true)}>
+                  <Text style={styles.touchablebutton}>Touchable button</Text>
+                </TouchableHighlight>
+                <Pressable onPress={() => console.warn("button pressed")} onLongPress={() => console.warn("long pressed")}
+                  onPressIn={() => console.warn("press in")} onPressOut={() => console.warn("press out")}>
+                  <Text style={styles.pressableBtn}>Pressable button</Text>
+                </Pressable>
+                <RadioButton />
+              </View>
+              <Text>{Platform.OS}</Text>
+            </View>
+            <View style={styles.box3}>
+              <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                {
+                  showCustomModal ? <CustomModal /> : null
+                }
+              </View>
+              <Button title='Next Screen' onPress={() => props.navigation.navigate('Login', { name: GetName, otherName: name })} />
+              <Button title="Open Custom modal" onPress={() => setShowCustomModal(!showCustomModal)} />
+              <Text style={[styles.textbox, ExStyles.textbox, { padding: 10, borderRadius: 10 }]}>{name}</Text>
+              <Text>{fruit()}</Text>
+              <Text>{3 * 2}</Text>
+              <Text>{age === 39 ? 'OldAge' : 'YoungAge'}</Text>
+              <Text> State Name : {GetName} </Text>
+              <CustomButton buttonTitle="Press Here" />
+              <Button title="Update Name" onPress={ChangeName}></Button>
               {
-                Platform.OS == 'android' ? <Text style={{ backgroundColor: 'blue' }}>hiii {Platform.OS}</Text> :
-                  <Text style={{ backgroundColor: 'purple' }}>Hii {Platform.OS}</Text>
+                GetName == 'Freya' ? <FirstComponent name={GetName} /> : null
               }
-            </View>
-          </View>
-          <View style={styles.box2}>
-            <View style={styles.innerbox21}>
-              <TouchableHighlight onPress={() => SetOpenWebView(true)}>
-                <Text style={styles.touchablebutton}>Touchable button</Text>
-              </TouchableHighlight>
-              <Pressable onPress={() => console.warn("button pressed")} onLongPress={() => console.warn("long pressed")}
-                onPressIn={() => console.warn("press in")} onPressOut={() => console.warn("press out")}>
-                <Text style={styles.pressableBtn}>Pressable button</Text>
-              </Pressable>
-              <RadioButton />
-            </View>
-            <Text>{Platform.OS}</Text>
-          </View>
-          <View style={styles.box3}>
-            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-              {
-                showCustomModal ? <CustomModal /> : null
-              }
-            </View>
-            <Button title='Next Screen' onPress={() => props.navigation.navigate('Login', { name: GetName, otherName: name })} />
-            <Button title="Open Custom modal" onPress={() => setShowCustomModal(!showCustomModal)} />
-            <Text style={[styles.textbox, ExStyles.textbox, { padding: 10, borderRadius: 10 }]}>{name}</Text>
-            <Text>{fruit()}</Text>
-            <Text>{3 * 2}</Text>
-            <Text>{age === 39 ? 'OldAge' : 'YoungAge'}</Text>
-            <Text> State Name : {GetName} </Text>
-            <CustomButton buttonTitle="Press Here" />
-            <Button title="Update Name" onPress={ChangeName}></Button>
-            {
-              GetName == 'Freya' ? <FirstComponent name={GetName} /> : null
-            }
 
-            <TextInput style={ExStyles.textinput} value={GetName} placeholder='Enter Your Name'
-              onChangeText={(text) => SetName(text)} />
-            <CustomButton buttonTitle="Clear Input" />
-            <TextInput style={ExStyles.textinput} value={GetPassword} placeholder='Enter Password'
-              onChangeText={(text) => Setpassword(text)}
-              secureTextEntry={DisplayPassword} />
-            <Button title='View Password' onPress={() => SetDisplayPassword(!DisplayPassword)}></Button>
-            <Button title='Clear Password' onPress={() => Setpassword('')}></Button>
-            <FlatList data={users} horizontal={true} renderItem={({ item }) => <Text style={styles.textbox}>{item.name}</Text>}
-              keyExtractor={item => item.id} />
-            <ScrollView>
-              <View style={styles.grid}>{
-                emails.map((item, index) => <Text key={index} style={styles.griditem}>{item.text}</Text>)
-              }</View>
-            </ScrollView>
-            <FlatList data={newemails} horizontal={true} renderItem={({ item }) => <GridData itemlist={item} />}
-              keyExtractor={item => item.id} />
-            <SectionList horizontal={true} sections={studentreport} renderItem={({ item }) => <Text>{item.subject} - {item.marks} </Text>}
-              renderSectionHeader={({ section: { name } }) => <Text>{name}</Text>} />
-            <Student name={GetName} />
+              <TextInput style={ExStyles.textinput} value={GetName} placeholder='Enter Your Name'
+                onChangeText={(text) => SetName(text)} ref={input} />
+              <CustomButton buttonTitle="Clear Input" />
+              <TextInput style={ExStyles.textinput} value={GetPassword} placeholder='Enter Password'
+                onChangeText={(text) => Setpassword(text)}
+                secureTextEntry={DisplayPassword} />
+              <Button title='View Password' onPress={() => SetDisplayPassword(!DisplayPassword)}></Button>
+              <Button title='Clear Password' onPress={() => Setpassword('')}></Button>
+              <FlatList data={users} horizontal={true} renderItem={({ item }) => <Text style={styles.textbox}>{item.name}</Text>}
+                keyExtractor={item => item.id} />
+              <ScrollView>
+                <View style={styles.grid}>{
+                  emails.map((item, index) => <Text key={index} style={styles.griditem}>{item.text}</Text>)
+                }</View>
+              </ScrollView>
+              <FlatList data={newemails} horizontal={true} renderItem={({ item }) => <GridData itemlist={item} />}
+                keyExtractor={item => item.id} />
+              <SectionList horizontal={true} sections={studentreport} renderItem={({ item }) => <Text>{item.subject} - {item.marks} </Text>}
+                renderSectionHeader={({ section: { name } }) => <Text>{name}</Text>} />
+              <Student name={GetName} />
+            </View>
           </View>
-        </View>
-      }
-    </View>
+        }
+      </View>
+    </ScrollView>
   );
 }
 
